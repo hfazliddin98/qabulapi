1d550b83b6e9b419742d4d83edc0648fb038a016
hfazliddin98 <hatamovfazliddin505@gmail.com>
2024-07-13 13:17:26 +0500

    13.7.2024

--- ariza/models.py
+++ ariza/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Ariza(models.Model):
-    davlat = models.CharField(max_length=255, blank=True)
-    viloyat = models.CharField(max_length=255, blank=True)
-    manzil = models.CharField(max_length=255, blank=True)
     familya = models.CharField(max_length=255, blank=True)
     ism = models.CharField(max_length=255, blank=True)
+    sharif = models.CharField(max_length=255, blank=True)
     email = models.CharField(max_length=255, blank=True)
     telefon_nomer = models.CharField(max_length=255, blank=True)
     tugatgan_oligoh = models.CharField(max_length=255, blank=True)
     fuqaroligi = models.CharField(max_length=255, blank=True)
     telegram_name = models.CharField(max_length=255, blank=True)
     whatsapp_name = models.CharField(max_length=255, blank=True)
+    pasport = models.FileField(upload_to='pasport', blank=True)
+    diplom = models.FileField(upload_to='diplom', blank=True)
     vaqt = models.DateTimeField(auto_now_add=True)
 
 
--- ariza/views.py
+++ ariza/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 from .models import Ariza, Qolanma
 from .serialazer import QolanmaSerializer, ArizaSerializer
 
 
 class ArizaViewSet(ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Ariza.objects.all()
     serializer_class = ArizaSerializer
 
